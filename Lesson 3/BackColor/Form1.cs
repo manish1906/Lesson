92ec6817cb950074c8aed2badc4ee2fa27b2ec05
manish106 <parmarmanishj2012@gmail.com>
2020-05-02 22:21:23 +0530

    lesson 3 added

---
+++ Lesson 3/BackColor/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureColor();
+        }
+        private void ConfigureColor()
+        {
+            try
+            {
+                string colorName = ConfigurationManager.
+                    AppSettings["BackColor"];
+                this.BackColor = Color.FromName(colorName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
