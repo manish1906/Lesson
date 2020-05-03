dd01908e4b8d2ca80a3e4bbfbdd161ca4fabd0fc
manish106 <parmarmanishj2012@gmail.com>
2020-05-03 18:18:08 +0530

    Lesson 3 added

---
+++ Lesson 3/MouseDownEvent/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseDownEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Text = String.Format(
+              "X = {0}, Y = {1}", e.X, e.Y);
+        }
+
+    }
+}
